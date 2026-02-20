03e4d5fcfba75e7c4f6aa378c4a231900c999c74
0xgleb <gleb.dianov@protonmail.com>
2026-02-20 19:31:02 +0400

    bootstrap remote

--- keys.nix
+++ keys.nix
@@ -3,7 +3,7 @@ rec {
     st0x-op =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPZ56nOYbGDd0ZfbqxeY7AbvaQGQrHnlC80ccpRGpCoj";
     host =
-      "ssh-ed25519 PLACEHOLDER";
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9JhlVsHGlSS3c+RGKFSwXyuFpvUTbnOny9e2AdBQ6G";
     ci =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIARWUchVuQvkFm2tzspdC79hhRyYbWzRjs5iimhxewUy";
     arda =
