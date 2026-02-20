89cf8fab2e96542fa54f754b499121785bc766de
0xgleb <gleb.dianov@protonmail.com>
2026-02-20 19:05:54 +0400

    add arda to keys.nix

--- keys.nix
+++ keys.nix
@@ -6,10 +6,12 @@ rec {
       "ssh-ed25519 PLACEHOLDER";
     ci =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIARWUchVuQvkFm2tzspdC79hhRyYbWzRjs5iimhxewUy";
+    arda =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyTREGZCOzMsl7N9dp1saN/t7DCs7YesusVUKApMJ78";
   };
 
   roles = {
     infra = [ keys.st0x-op keys.ci ];
-    ssh = [ keys.st0x-op keys.ci ];
+    ssh = [ keys.st0x-op keys.ci keys.arda ];
   };
 }
